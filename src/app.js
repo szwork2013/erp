288e2f789660bdc8687cc40cad095b5b422d2b6e
Dries De Rudder <dries.derudder@gmail.com>
2014-08-02 17:23:54 +0200

    adding settings file for app-port number

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var server = require('./server/server.js');
 
+var settings = require('./settings.json');
+
 var app = express([]);
 
 app.use(express.static(__dirname + '/client'));
-
 app.use('/api', server());
 
-app.listen(8080);
+app.listen(settings.port);
