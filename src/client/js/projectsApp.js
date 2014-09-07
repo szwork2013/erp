dbd5aa9a17315c18ad447ec60148f296a6ba44cf
Dries De Rudder <dries.derudder@gmail.com>
2014-09-07 19:33:22 +0200

    time registration client

--- src/client/js/projectsApp.js
+++ src/client/js/projectsApp.js
@@ -1,55 +1,55 @@
 var projectsApp = angular.module('ProjectsApp', ['ngRoute']);
 
-projectsApp.config(['$routeProvider', function($routeProvider) {
-	$routeProvider
-		.when('/overview/:page?', {
-			templateUrl: 'projects/index.partial.html',
-			controller: 'ProjectsOverviewController'
+projectsApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+		.when('/overview', {
+		    templateUrl: 'projects/index.partial.html',
+		    controller: 'ProjectsOverviewController'
 		})
 		.when('/create', {
-			templateUrl: 'projects/create.partial.html',
-			controller: 'CreateProjectController'
+		    templateUrl: 'projects/create.partial.html',
+		    controller: 'CreateProjectController'
 		})
 		.when('/:id', {
-			templateUrl: 'projects/details.partial.html',
-			controller: 'ProjectDetailController'
+		    templateUrl: 'projects/details.partial.html',
+		    controller: 'ProjectDetailController'
 		})
 		.when('/:id/workitems', {
-			templateUrl: 'projects/workitem/index.partial.html',
-			controller: 'ProjectWorkItemsController'
+		    templateUrl: 'projects/workitem/index.partial.html',
+		    controller: 'ProjectWorkItemsController'
 		})
 		.when('/:id/workitems/create', {
-			templateUrl: 'projects/workitem/create.partial.html',
-			controller: 'ProjectCreateWorkItemController'
+		    templateUrl: 'projects/workitem/create.partial.html',
+		    controller: 'ProjectCreateWorkItemController'
 		})
 		.when('/:id/workitems/:workitemid', {
-			templateUrl: 'projects/workitem/detail.partial.html',
-			controller: 'ProjectWorkItemDetailController'
+		    templateUrl: 'projects/workitem/detail.partial.html',
+		    controller: 'ProjectWorkItemDetailController'
 		})
 		.when('/:id/workitems/:workitemid/materials', {
-			templateUrl: 'projects/workitem/materials.partial.html',
-			controller: 'ProjectWorkItemMaterialsController'
+		    templateUrl: 'projects/workitem/materials.partial.html',
+		    controller: 'ProjectWorkItemMaterialsController'
 		})
 		.when('/:id/workitems/:workitemid/tasks', {
-			templateUrl: 'projects/workitem/tasks.partial.html',
-			controller: 'ProjectWorkItemTasksController'
+		    templateUrl: 'projects/workitem/tasks.partial.html',
+		    controller: 'ProjectWorkItemTasksController'
 		})
 		.otherwise({
-			redirectTo: '/overview'
+		    redirectTo: '/overview'
 		});
-}]);
+} ]);
 
 projectsApp.controller('ProjectsOverviewController', [
 	'$scope',
 	'$http',
 	'$routeParams',
-	function($scope, $http, $routeParams) {
-		var page = $routeParams.page || 0;
-		
-		var projectsRequest = $http.get('/api/projects?page=' + page);
-		projectsRequest.success(function(data) {
-			$scope.projects = data;
-		});
+	function ($scope, $http, $routeParams) {
+	    var page = $routeParams.page || 0;
+
+	    var projectsRequest = $http.get('/api/projects?page=' + page);
+	    projectsRequest.success(function (data) {
+	        $scope.projects = data;
+	    });
 	}
 ]);
 
@@ -57,23 +57,23 @@ projectsApp.controller('CreateProjectController', [
 	'$scope',
 	'$http',
 	'$location',
-	function($scope, $http, $location) {
-		$scope.create = function() {
-			$scope.error = '';
-			
-			if (!$scope.name) {
-				$scope.error = "Kies een naam voor het nieuwe project";
-			}
-			else {
-				var createRequest = $http.put('/api/projects', { name : $scope.name});
-				createRequest.success(function(data) {
-					$location.path('/' + data.id);
-				})
-				.error(function(data, status) {
-					alert(status + ' ' + data);
+	function ($scope, $http, $location) {
+	    $scope.create = function () {
+	        $scope.error = '';
+
+	        if (!$scope.name) {
+	            $scope.error = "Kies een naam voor het nieuwe project";
+	        }
+	        else {
+	            var createRequest = $http.put('/api/projects', { name: $scope.name });
+	            createRequest.success(function (data) {
+	                $location.path('/' + data.id);
+	            })
+				.error(function (data, status) {
+				    alert(status + ' ' + data);
 				});
-			}
-		};
+	        }
+	    };
 	}
 ]);
 
@@ -82,17 +82,17 @@ projectsApp.controller('ProjectDetailController', [
 	'$http',
 	'$routeParams',
 	function ($scope, $http, $routeParams) {
-		var projectRequest = $http.get('/api/projects/' + $routeParams.id);
-		projectRequest.success(function(data) {
-			$scope.project = data;
-			
-			$scope.save = function() {
-				var saveRequest = $http.post('/api/projects', $scope.project);
-				saveRequest.success(function(data) {
-					$scope.saveconfirm = "Wijzigingen opgeslaan";
-				});
-			};
-		});
+	    var projectRequest = $http.get('/api/projects/' + $routeParams.id);
+	    projectRequest.success(function (data) {
+	        $scope.project = data;
+
+	        $scope.save = function () {
+	            var saveRequest = $http.post('/api/projects', $scope.project);
+	            saveRequest.success(function (data) {
+	                $scope.saveconfirm = "Wijzigingen opgeslaan";
+	            });
+	        };
+	    });
 	}
 ]);
 
@@ -101,44 +101,44 @@ projectsApp.controller('ProjectWorkItemsController', [
 	'$http',
 	'$routeParams',
 	function ($scope, $http, $routeParams) {
-		var projectRequest = $http.get('/api/projects/' + $routeParams.id);
-		projectRequest.success(function(data) {
-			$scope.project = data;
-		});
-		
-		var workitemsRequest = $http.get('/api/projects/' + $routeParams.id + '/workitems');
-		workitemsRequest.success(function(data) {
-			$scope.workitems = data;
-		});
+	    var projectRequest = $http.get('/api/projects/' + $routeParams.id);
+	    projectRequest.success(function (data) {
+	        $scope.project = data;
+	    });
+
+	    var workitemsRequest = $http.get('/api/projects/' + $routeParams.id + '/workitems');
+	    workitemsRequest.success(function (data) {
+	        $scope.workitems = data;
+	    });
 	}
 ]);
 
 projectsApp.controller('ProjectCreateWorkItemController', [
 	'$scope',
-	'$http', 
+	'$http',
 	'$routeParams',
 	'$location',
-	function($scope, $http, $routeParams, $location) {
-		$scope.name = '';
-		$scope.description = '';
-	
-		var projectRequest = $http.get('/api/projects/' + $routeParams.id);
-		projectRequest.success(function(data) {
-			$scope.project = data;
-			
-			$scope.save = function() {
-				var workItem = {
-					projectId: $scope.project.id,
-					name: $scope.name,
-					description: $scope.description
-				};
-				
-				var createRequest = $http.put('/api/projects/workitems', workItem);
-				createRequest.success(function(data) {
-					$location.path('/' + $scope.project.id + '/workitems');
-				});
-			};
-		});
+	function ($scope, $http, $routeParams, $location) {
+	    $scope.name = '';
+	    $scope.description = '';
+
+	    var projectRequest = $http.get('/api/projects/' + $routeParams.id);
+	    projectRequest.success(function (data) {
+	        $scope.project = data;
+
+	        $scope.save = function () {
+	            var workItem = {
+	                projectId: $scope.project.id,
+	                name: $scope.name,
+	                description: $scope.description
+	            };
+
+	            var createRequest = $http.put('/api/projects/workitems', workItem);
+	            createRequest.success(function (data) {
+	                $location.path('/' + $scope.project.id + '/workitems');
+	            });
+	        };
+	    });
 	}
 ]);
 
@@ -146,20 +146,20 @@ projectsApp.controller('ProjectWorkItemDetailController', [
 	'$scope',
 	'$http',
 	'$routeParams',
-	function($scope, $http, $routeParams) {
-		var projectRequest = $http.get('/api/projects/' + $routeParams.id);
-		projectRequest.success(function(data){
-			$scope.project = data;
-		});
-		
-		var workItemRequest = $http.get('/api/projects/' + $routeParams.id + '/workitems/' + $routeParams.workitemid);
-		workItemRequest.success(function(data) {
-			$scope.workitem = data;
-		});
+	function ($scope, $http, $routeParams) {
+	    var projectRequest = $http.get('/api/projects/' + $routeParams.id);
+	    projectRequest.success(function (data) {
+	        $scope.project = data;
+	    });
+
+	    var workItemRequest = $http.get('/api/projects/' + $routeParams.id + '/workitems/' + $routeParams.workitemid);
+	    workItemRequest.success(function (data) {
+	        $scope.workitem = data;
+	    });
 	}
 ]);
 
-	projectsApp.controller('ProjectWorkItemMaterialsController', [
+projectsApp.controller('ProjectWorkItemMaterialsController', [
 	'$scope',
 	'$http',
 	'$routeParams',
@@ -190,7 +190,7 @@ projectsApp.controller('ProjectWorkItemDetailController', [
 	}
 ]);
 
-	projectsApp.controller('ProjectWorkItemTasksController', [
+projectsApp.controller('ProjectWorkItemTasksController', [
 	'$scope',
 	'$http',
 	'$routeParams',
---
+++ src/client/js/timesheetsApp.js
@@ -0,0 +1,54 @@
+var timesheetsApp = angular.module('TimesheetsApp', ['ngRoute', 'ui.bootstrap']);
+
+timesheetsApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+		.when('/overview', {
+		    templateUrl: 'timesheets/overview.partial.html',
+		    controller: 'OverviewController'
+		})
+        .when('/input', {
+            templateUrl: 'timesheets/input.partial.html',
+            controller: 'InputController'
+        })
+		.otherwise({
+		    redirectTo: '/overview'
+		});
+} ]);
+
+timesheetsApp.controller('OverviewController', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
+        $scope.something = 'test';
+    }
+]);
+
+    timesheetsApp.controller('InputController', [
+    '$scope',
+    '$http',
+    function ($scope, $http) {
+        $scope.employee = undefined;
+        $scope.project = undefined;
+
+        $scope.employees = [];
+        var employeesRequest = $http.get('/api/contacts/Werknemer');
+        employeesRequest.success(function (data) {
+            $scope.employees = data;
+        });
+
+        $scope.projects = [];
+        var projectsRequest = $http.get('/api/projects');
+        projectsRequest.success(function (data) {
+            $scope.projects = data;
+        });
+
+        $scope.add = function () {
+            var tr = {};
+            tr.employee = $scope.employee._id;
+            tr.date = $scope.date;
+            tr.project = $scope.project.id;
+            tr.hours = $scope.hours;
+            tr.description = $scope.description;
+        };
+    }
+]);
